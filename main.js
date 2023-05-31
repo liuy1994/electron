16125c36bcdc5f57a16eb9374b892d4b5e9ce7da
liam <liam.liu@wayz.ai>
2023-05-31 17:45:37 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron')
+// 需在当前文件内开头引入 Node.js 的 'path' 模块
+const path = require('path')
+
+const createWindow = () => {
+  const mainWindow  = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  mainWindow .loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
