19b04797a61da8d356649c374df3d6b0539abc68
liam <liam.liu@wayz.ai>
2023-06-01 14:36:43 +0800

    feat: 通信

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 require('update-electron-app')()
-const { app, BrowserWindow, ipcMain } = require('electron')
+const { app, BrowserWindow, ipcMain, dialog, Menu } = require('electron')
 const path = require('path')
 
 const createWindow = () => {
@@ -10,9 +10,46 @@ const createWindow = () => {
       preload: path.join(__dirname, 'preload.js')
     }
   })
-  ipcMain.handle("ping", () => "PING")
+
+  const menu = Menu.buildFromTemplate([
+    {
+      label: app.name,
+      submenu: [
+        {
+          click: () => mainWindow.webContents.send('update-counter', 1),
+          label: 'Increment'
+        },
+        {
+          click: () => mainWindow.webContents.send('update-counter', -1),
+          label: 'Decrement'
+        }
+      ]
+    }
+  ])
+  Menu.setApplicationMenu(menu)
+
+
   mainWindow.loadFile('index.html')
   mainWindow.webContents.openDevTools()
+}
+
+const handleSetTitle = (event, title) => {
+  const webContent = event.sender
+  const win = BrowserWindow.fromWebContents(webContent)
+  win.setTitle(title)
+}
+
+const handlePing = () => {
+  return new Promise(resolve => {
+    setTimeout(() => resolve("PING"), 2000)
+  })
+}
+
+ const handleFileOpen = async () => {
+  const { canceled, filePaths } = await dialog.showOpenDialog()
+  if (!canceled) {
+    return filePaths[0]
+  }
 }
 
 app.whenReady().then(() => {
@@ -20,6 +57,15 @@ app.whenReady().then(() => {
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
+  // 单向通信，没有返回
+  ipcMain.on("set-title", handleSetTitle)
+  ipcMain.on("counter-value", (_event, value) => {
+    console.log(value)
+  })
+
+  // 双向通信，有返回
+  ipcMain.handle("ping", handlePing)
+  ipcMain.handle("dialog:openFile", handleFileOpen)
 })
 
 app.on('window-all-closed', () => {
--- preload.js
+++ preload.js
@@ -4,6 +4,11 @@ contextBridge.exposeInMainWorld('versions', {
   node: () => process.versions.node,
   chrome: () => process.versions.chrome,
   electron: () => process.versions.electron,
-  ping: () => ipcRenderer.invoke('ping')
   // we can also expose variables, not just functions
+})
+contextBridge.exposeInMainWorld("electronAPI", {
+  ping: () => ipcRenderer.invoke('ping'),
+  openFile: () => ipcRenderer.invoke('dialog:openFile'),
+  setTitle: (title) => ipcRenderer.send('set-title', title),
+  onUpdateCounter: callback => ipcRenderer.on("update-counter", callback),
 })
